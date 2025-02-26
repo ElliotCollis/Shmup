29ea94d128d7df9747f930ed1d3b49eb18e78469
Elliot Collis <elliot@Elliots-MacBook-Air.local>
2025-02-26 15:36:25 +0900

    update : comments

--- Assets/Scripts/CanvasManager.cs
+++ Assets/Scripts/CanvasManager.cs
@@ -6,7 +6,7 @@ public class CanvasManager : MonoBehaviour // simple script for swapping UI canv
 {
     public void SwapCanvas (GameObject TurnOff, GameObject TurnOn) // takes in one canvas to turn off and one to turn on.
     {
-        TurnOff?.SetActive(false);
+        TurnOff?.SetActive(false); // here we use the null-conditional operator instead of an if null reference check. 
         TurnOn?.SetActive(true);
     }
 }
